810e69959a5a08b32d16ff342e1a32b9bdc70852
akhena1 <gu.marcondes2012@gmail.com>
2020-01-14 12:00:27 -0300

    conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://DevTestServer:<lo4wywPl1vYAwpLm>@cluster0-shard-00-00-yaai8.mongodb.net:27017,cluster0-shard-00-01-yaai8.mongodb.net:27017,cluster0-shard-00-02-yaai8.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
@@ -11,6 +17,8 @@ app.use(express.json());
 //Query Params: req.query (Filtros, ordenação, paginação)
 //Route Params: req.params (Identificar um recurso na alteração ou remoção)
 //Body:         req.body (dados para a criação ou alteração de um registro)
+
+// MongoDB (Banco de dados não-relacional)
 
 app.get('/users', (req, res) => {
     return res.json({ message: 'Hello Omnistack' });
