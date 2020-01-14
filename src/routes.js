59ebb14885e2e56713ebf25ea373b279ea902374
akhena1 <gu.marcondes2012@gmail.com>
2020-01-14 17:07:50 -0300

    Abstraindo a requisição para uma controller

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,10 @@
 const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+
+
 const routes = Router();
-const axios = require('axios');
-const Dev = require('./models/Dev')
 
-routes.post('/devs', async (req, res) => {
 
-    const{ github_username, techs, latitude, longitude } = req.body
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-    
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const TechsArray = techs.split(',').map (techs => techs.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude , latitude]
-    }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: TechsArray,
-        location
-    })
-
-    return res.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
