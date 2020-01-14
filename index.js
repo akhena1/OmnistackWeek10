6cf083e6f173e7228b32ab1790152afcb2a9d1ee
akhena1 <gu.marcondes2012@gmail.com>
2020-01-14 12:12:26 -0300

    correção da string de conexão com o banco

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://DevTestServer:<lo4wywPl1vYAwpLm>@cluster0-shard-00-00-yaai8.mongodb.net:27017,cluster0-shard-00-01-yaai8.mongodb.net:27017,cluster0-shard-00-02-yaai8.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://DevTestServer:1234@cluster0-yaai8.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 });
 
 app.use(express.json());
