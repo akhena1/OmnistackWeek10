5553e9d763eafe47c30b75ddb61e149948ae73a6
akhena1 <gu.marcondes2012@gmail.com>
2020-01-14 11:41:19 -0300

    declando  o uso de JSON dentro do express

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+// Métodos HTTP : get, post, put, delete
+
+//Tipos de parâmetros:
+
+//Query Params: req.query (Filtros, ordenação, paginação)
+//Route Params: req.params (Identificar um recurso na alteração ou remoção)
+//Body:         req.body (dados para a criação ou alteração de um registro)
+
+app.get('/users', (req, res) => {
     return res.json({ message: 'Hello Omnistack' });
 });
 
